34425b09fee62a37e1afd9f6083cbcfba047f1ff
DESKTOP-VDFLBFT\user <huozuyinshua@gmail.com>
2021-05-31 09:30:17 +0900

    Create 11052.py

---
+++ 11052/11052.py
@@ -0,0 +1,18 @@
+M = int(input())
+cardList = [int(x) for x in input().split()]
+
+dp = [0]*M
+
+for i in range(len(cardList)):
+    if i == 0:
+        dp[0] = cardList[0]
+    else:
+        ma = cardList[i]
+        for j in range(i):
+            temp = dp[i-1-j]+dp[j]
+            if temp > ma:
+                ma = temp
+        dp[i] = ma
+print(dp[-1])
+
+
